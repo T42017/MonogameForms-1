a2aca6532436ef24cf73278726f1d39041dffdcf
Måns H <mans.holmgren@lund.ntig.se>
2017-12-05 11:17:58 +0100

    Fix

--- MonoGameForms/MonoFormsLibrary/DrawableBaseComponent.cs
+++ MonoGameForms/MonoFormsLibrary/DrawableBaseComponent.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace MonoFormsLibrary
 {
-    class DrawableBaseComponent
+    public class DrawableBaseComponent : DrawableGameComponent
     {
+        public DrawableBaseComponent(Game game) : base(game)
+        {
+        }
+
+        public SpriteBatch SpriteBatch { get; private set; }
+
+        public virtual void Remove()
+        {
+            Game.Components.Remove(this);
+        }
+
+        protected override void LoadContent()
+        {
+            SpriteBatch = new SpriteBatch(Game.GraphicsDevice);
+            base.LoadContent();
+        }
+
+        
     }
 }
